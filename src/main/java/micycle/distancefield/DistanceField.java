276789cd1f4a79c0a42b8d3242e0ca764305c6e0
micycle1 <micycle1@hotmail.co.uk>
2021-04-28 17:28:03 +0100

    isoline

--- src/main/java/micycle/distancefield/DistanceField.java
+++ src/main/java/micycle/distancefield/DistanceField.java
@@ -5,19 +5,27 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 import org.jgrapht.Graph;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
+import org.locationtech.jts.algorithm.Orientation;
+import org.locationtech.jts.geom.Coordinate;
+import org.tinfour.common.IConstraint;
 import org.tinfour.common.IQuadEdge;
+import org.tinfour.common.PolygonConstraint;
 import org.tinfour.common.Vertex;
 import org.tinfour.interpolation.IInterpolatorOverTin;
 import org.tinfour.interpolation.NaturalNeighborInterpolator;
 import org.tinfour.standard.IncrementalTin;
+import org.tinfour.utils.TriangleCollector;
 
 import micycle.distancefield.interpolator.NNIF;
 import micycle.distancefield.interpolator.TFIF;
+import micycle.pgs.PGS_Conversion;
 import micycle.pgs.PGS_Triangulation;
+
 import peasyGradients.gradient.Gradient;
 import processing.core.PApplet;
 import processing.core.PGraphics;
@@ -70,7 +78,8 @@ public class DistanceField {
 		THREAD_COUNT = Math.min(4, Runtime.getRuntime().availableProcessors() - 1);
 		THREAD_POOL = Executors.newFixedThreadPool(THREAD_COUNT);
 
-		tin = PGS_Triangulation.delaunayTriangulationTin(shape, null, true, 0, true);
+		final int REFINEMENTS = 0;
+		tin = PGS_Triangulation.delaunayTriangulationTin(shape, null, true, REFINEMENTS, true);
 
 		graph = new DefaultUndirectedWeightedGraph<>(IQuadEdge.class);
 		tin.edges().forEach(e -> {
@@ -105,8 +114,9 @@ public class DistanceField {
 	 * 
 	 * @param interpolator
 	 * @param origin
+	 * @return
 	 */
-	public void compute(Interpolator interpolator, final PVector origin) {
+	public void computeField(Interpolator interpolator, final PVector origin) {
 		IncrementalTin distanceMesh = new IncrementalTin(10);
 
 		shortestPaths = new DijkstraShortestPath<>(graph);
@@ -164,11 +174,144 @@ public class DistanceField {
 		p.ellipse((float) originVertex.x, (float) originVertex.y, 10, 10);
 	}
 
+	/**
+	 * Compute the distance line for the shape using the vertex of the PShape
+	 * CLOSEST to given origin point.
+	 * 
+	 * @param interpolator
+	 * @param origin
+	 * @return
+	 */
+	public PShape computeIsoline(Interpolator interpolator, final PVector origin, double distance) {
+		IncrementalTin distanceMesh = new IncrementalTin(10);
+		IncrementalTin constrainedMesh = new IncrementalTin(10);
+
+		shortestPaths = new DijkstraShortestPath<>(graph);
+		var originVertex = tin.getNavigator().getNearestVertex(origin.x, origin.y); // vertices only
+		double maxDistance = 0;
+		if (graph.containsVertex(originVertex)) {
+			var paths = shortestPaths.getPaths(originVertex);
+			for (Vertex v : graph.vertexSet()) {
+				var d = paths.getWeight(v);
+				distanceMesh.add(new Vertex(v.x, v.y, d));
+				constrainedMesh.add(new Vertex(v.x, v.y, d));
+				maxDistance = Math.max(maxDistance, d);
+			}
+		}
+
+		List<IConstraint> constraints = new ArrayList<>();
+		var ring = PGS_Conversion.fromPShape(shape);
+		ArrayList<Vertex> points = new ArrayList<>();
+		Coordinate[] c = ring.getCoordinates();
+		if (Orientation.isCCW(c)) {
+			for (int i = 0; i < c.length; i++) {
+				points.add(new Vertex(c[i].x, c[i].y, 0));
+			}
+		} else {
+			for (int i = c.length - 1; i >= 0; i--) { // iterate backwards if CW
+				points.add(new Vertex(c[i].x, c[i].y, 0));
+			}
+		}
+		constraints.add(new PolygonConstraint(points));
+
+		constrainedMesh.addConstraints(constraints, true); // true/false is negligible?
+
+		PShape isoline = new PShape(PShape.GEOMETRY);
+		isoline.setStroke(true);
+		isoline.setStrokeCap(PShape.ROUND);
+		isoline.setStroke(p.color(255, 0, 0));
+		isoline.setStrokeWeight(10);
+
+		isoline.beginShape(PShape.LINES);
+		TriangleCollector.visitTrianglesConstrained(constrainedMesh, new Consumer<Vertex[]>() {
+
+			@Override
+			public void accept(Vertex[] t) {
+
+				if (t[0].isConstraintMember() || t[1].isConstraintMember() || t[2].isConstraintMember()) {
+					return;
+				}
+				p.beginShape(PShape.LINES);
+				p.stroke(PApplet.map((float) t[0].x, 0, p.width, 0, 255), 20,
+						PApplet.map((float) t[0].y, 0, p.width, 255, 0));
+
+				int n = 0;
+				if (isoVertex(t[0], t[1], isoline, distance)) {
+					n++;
+				}
+				if (isoVertex(t[1], t[2], isoline, distance)) {
+					n++;
+				}
+				if (isoVertex(t[2], t[0], isoline, distance)) {
+					n++;
+				}
+				if (n == 2) { // isoline crosses triangle
+//					drawTriangle(t[0], t[1], t[2]);
+				}
+				p.endShape();
+			}
+		});
+
+		isoline.endShape();
+
+		return isoline;
+	}
+
+	/**
+	 * Compute (if possible) the coordinate where the isoline crosses the between
+	 * the two vertices.
+	 * 
+	 * @return
+	 */
+	private boolean isoVertex(Vertex a, Vertex b, PShape s, double d) {
+		Vertex min, max;
+
+		if (a.getZ() > b.getZ()) {
+			max = a;
+			min = b;
+		} else {
+			max = b;
+			min = a;
+		}
+
+		if (d > min.getZ() && d < max.getZ()) {
+			double diff = max.getZ() - min.getZ();
+			double numerator = d - min.getZ();
+
+			double fract = numerator / diff;
+			double xDiff = max.getX() - min.getX();
+			double yDiff = max.getY() - min.getY();
+			p.vertex((float) (min.getX() + fract * xDiff), (float) (min.getY() + fract * yDiff));
+//			return new PVector((float) (min.getX() + fract * xDiff), (float) (min.getY() + fract * yDiff));
+			return true;
+		}
+		return false;
+	}
+
+	private void drawTriangle(Vertex a, Vertex b, Vertex c) {
+		var v = a;
+		if (v != null && b != null && c != null) {
+			p.fill(120, PApplet.map((float) a.x, 0, p.width, 0, 255), PApplet.map((float) a.y, 0, p.height, 0, 255));
+			p.stroke(120, PApplet.map((float) a.x, 0, p.width, 0, 255), PApplet.map((float) a.y, 0, p.height, 0, 255));
+			p.beginShape();
+			p.strokeWeight(1);
+			p.vertex((float) v.x, (float) v.y);
+			v = b;
+			p.vertex((float) v.x, (float) v.y);
+			v = c;
+			p.vertex((float) v.x, (float) v.y);
+			p.endShape();
+		}
+	}
+
 	// get array[][] of normalised distance field values (not colors)
 	public void getField() {
 
 	}
 
+	/**
+	 * Public to be called by PApplet when window closed.
+	 */
 	public void dispose() {
 		THREAD_POOL.shutdownNow();
 	}
@@ -179,11 +322,13 @@ public class DistanceField {
 		private final int startRow, rows;
 
 		/**
-		 * Each thread should get its own interpoaltor due to last-triangle caching
+		 * Each thread should get its own interpoaltor due to last-triangle caching.
+		 * Threads draw into rows that span the whole width of the PApplet.
 		 * 
 		 * @param interpolator
-		 * @param startRow
-		 * @param rows
+		 * @param startRow     index starting row for this thread
+		 * @param rows         number of rows this thread should calculate and render
+		 *                     for
 		 */
 		Thread(IInterpolatorOverTin interpolator, int startRow, int rows) {
 			this.interpolator = interpolator;
--- src/main/java/micycle/distancefield/DistanceFieldExample.java
+++ src/main/java/micycle/distancefield/DistanceFieldExample.java
@@ -1,10 +1,7 @@
 package micycle.distancefield;
 
 import micycle.distancefield.DistanceField.Interpolator;
-import micycle.pgs.PGS;
-import micycle.pgs.PGS_Morphology;
 import micycle.pgs.PGS_Processing;
-import micycle.pgs.PGS_Transformation;
 import micycle.pgs.utility.PoissonDistribution;
 import peasyGradients.gradient.Gradient;
 import processing.core.PApplet;
@@ -26,26 +23,35 @@ public final class DistanceFieldExample extends PApplet {
 	@Override
 	public void settings() {
 		System.setProperty("prism.allowhidpi", "false");
-		size(800, 800, FX2D);
+		size(1000, 1000, FX2D);
+		smooth(2);
 	}
+
+	int n = 0;
 
 	@Override
 	public void setup() {
+		frameRate(30);
 		heatMap = new Gradient();
 		heatMap.pushColor(color(20, 11, 52));
 		heatMap.pushColor(color(132, 32, 107));
 		heatMap.pushColor(color(229, 92, 48));
 		heatMap.pushColor(color(255, 243, 99));
-//		heatMap.primeAnimation();
+		heatMap.primeAnimation();
 		heatMap.prime();
 
 		PoissonDistribution pd = new PoissonDistribution(0);
 		final int buffer = 30;
 		var points = pd.generate(buffer, buffer, width - buffer, height - buffer, 25, 10);
-		shape = PGS_Processing.concaveHull(points, 500);
+		points.clear();
+		randomSeed(12);
+		for (int i = 0; i < 200; i++) {
+			points.add(new PVector(random(buffer, width - buffer), random(buffer, height - buffer)));
+		}
+		shape = PGS_Processing.concaveHull(points, 65);
 
-		var square = createShape(RECT, 0, 0, 300, 300);
-		square = PGS_Transformation.translateTo(square, width / 2, height / 2);
+//		var square = createShape(RECT, 0, 0, 300, 300);
+//		square = PGS_Transformation.translateTo(square, width / 2, height / 2);
 
 //		shape = PTS.difference(shape, square);
 		df = new DistanceField(this, shape, heatMap);
@@ -70,10 +76,28 @@ public final class DistanceFieldExample extends PApplet {
 	@Override
 	public void draw() {
 		background(0, 0, 40);
-		df.compute(Interpolator.Trianglular, new PVector(mouseX, mouseY));
-//		heatMap.animate(-0.01f);
-		image(shapeCache, 0, 0);
+
+		// ISOLINE
+//		shape(shape);
+//		image(shapeCache, 0, 0);
+//		var d = mouseX;
+//		PShape s = df.computeIsoline(Interpolator.Trianglular, new PVector(320, 320), d);
+//		shape(s);
+		// draw isoline distance circle
+//		strokeWeight(8);
+//		noFill();
+//		stroke(0, 255, 0);
+//		ellipse(320, 320, d * 2, d * 2);
+//		if (frameCount > 30) {
+//			n += 8;
+//		}
+
+		// DISTANCE FIELD
+		heatMap.animate(-0.01f);
+		df.computeField(Interpolator.Trianglular, new PVector(mouseX, mouseY));
 		text(frameRate, 0, 0);
+		image(shapeCache, 0, 0);
+
 	}
 
 }
